f02c69870cc5ab37bb06b56b6bcb503e37d91d59
Radex55 <radex55@users.noreply.github.com>
2024-07-23 09:09:17 +0700

    Delete application directory old file

--- application/config/hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-$hook['pre_system'][] = array(
-    'class'    => 'Preloadsystem',
-    'function' => 'validateUserAgent',
-    'filename' => 'security_check.php',
-    'filepath' => 'hooks'
-);
