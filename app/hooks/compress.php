737946c692749586c06bee49caeee7ae4bc0836f
Radex55 <radex55@users.noreply.github.com>
2024-08-16 13:58:48 +0700

    file compress output html CI

---
+++ app/hooks/compress.php
@@ -0,0 +1,35 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Minifyhtml {
+    
+    function output(){
+        $CI =& get_instance();
+        $buffer = $CI->output->get_output();
+        $re = '%            # Collapse ws everywhere but in blacklisted elements.
+            (?>             # Match all whitespans other than single space.
+              [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+            | \s{2,}        # or two or more consecutive-any-whitespace.
+            ) # Note: The remaining regex consumes no text at all...
+            (?=             # Ensure we are not in a blacklist tag.
+              (?:           # Begin (unnecessary) group.
+                (?:         # Zero or more of...
+                  [^<]++    # Either one or more non-"<"
+                | <         # or a < starting a non-blacklist tag.
+                            # Skip Script and Style Tags
+                  (?!/?(?:textarea|pre|script|style)\b)
+                )*+         # (This could be "unroll-the-loop"ified.)
+              )             # End (unnecessary) group.
+              (?:           # Begin alternation group.
+                <           # Either a blacklist start tag.
+                            # Dont foget the closing tags 
+                (?>textarea|pre|script|style)\b
+              | \z          # or end of file.
+              )             # End alternation group.
+            )  # If we made it here, we are not in a blacklist tag.
+            %ix';
+        $buffer = preg_replace($re, " ", $buffer);
+        $CI->output->set_output($buffer);
+        $CI->output->_display();
+    }
+}
+?>
